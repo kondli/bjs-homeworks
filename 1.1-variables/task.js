2e102d741a43e9ece30cce0646b7d614fcb9131a
Kondratskaia Liliia <lilu.sevast@gmail.com>
2020-12-27 09:12:57 +0300

    Added function to the 1.1-variables/task.js

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -4,24 +4,34 @@
 	2) После ключевого слова return вместо комментария необходимо написать переменную либо выражение с ответом.
 	3) console.log() прописывать не обязательно, т.к. команда return уже означает вывод результата работы функции.
  */
+let algebra;
+let geography;
+let physics;
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+    let result =(a + g + p)/3;
+	return result;
 }
 
+averageMark(algebra, geography, physics);
+
+let myName;
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	 let greeting = `Привет, мир! Меня зовут ${userName}`;
+	return greeting;
 }
 
+sayHello(myName);
+
+
+let x = 2;
+let y = 22;
+let z = 0;
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let result = x * y + 5 * z + x - 1;
+    return result;
 }
+
+calculateFormula();
